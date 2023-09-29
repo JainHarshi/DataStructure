7947f7ff424c8f9e0c05e66fcedb3f788258cee9
Harshi Jain <99419195+JainHarshi@users.noreply.github.com>
2023-09-29 13:46:07 +0530

    Add files via upload

---
+++ Testing.java
@@ -0,0 +1,10 @@
+package HASHING;
+
+public class Testing {
+    public static void main(String[] args) {
+//        CHARACTER CONVERTING INTO ASCII VALUE AND THEN MULTIPLY
+//        System.out.println('1'*2);
+        System.out.println(((char)'1')*2);
+
+    }
+}
